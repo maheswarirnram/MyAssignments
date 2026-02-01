9a2dc7b16028bc22adbe9e0f3f13f930bb8e122f
maheswarirnram <maheswarirnram@gmail.com>
2026-02-01 14:19:43 +0530

    Add files via upload
    Home Assignment Week 1_Sat

---
+++ EmployeeDetails.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		byte age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+
+		System.out.println("Employee Name: " + name + "\n" + "Employee Age: " + age + "\n" + "Employee Salary: " + salary
+				+ "\n" + "Employee Gender: " + gender + "\n" + "Employee Accept Terms: " + acceptTerms + "\n"
+				+ "Employee Rating: " + rating + "\n" + "Employee Mobile Number: " + mobileNumber + "\n"
+				+ "Employee Pincode: " + pinCode + "\n" + "Employee Lucky Number: " + luckyNumber);
+
+	}
+
+}
